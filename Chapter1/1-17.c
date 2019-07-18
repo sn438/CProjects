fda22dbb5b59de87a3eae126712c794e2d2df08b
sn438 <sn438@cornell.edu>
2019-07-18 18:02:25 -0400

    1-16 and 1-17

---
+++ Chapter1/1-17.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define MAXLINE 80 /* maximum input line length */
+int getlinea(char line[], int maxline);
+void copy(char to[], char from[]);
+/* print the longest input line */
+int main(void)
+{
+	int len; /* current line length */
+	int max; /* maximum length seen so far */
+	char line[MAXLINE]; /* current input line */
+	char longest[MAXLINE]; /* longest line saved here */
+	max = 0;
+	while ((len = getlinea(line, MAXLINE)) > 0);
+	return 0;
+}
+/* getline: read a line into s, return length */
+int getlinea(char s[],int lim)
+{
+	int c, i;
+	for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+	if (i = lim - 1){
+		while((c = getchar()) != EOF && c != '\n'){
+			i++;
+			
+		}
+	}
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+/* copy: copy ’from’ into ’to’; assume to is big enough */
+void copy(char to[], char from[])
+{
+	int i;
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
