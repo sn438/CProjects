31a970e6b20220ef3be2f6e6258ce0d845a2bfdd
sn438 <sn438@cornell.edu>
2019-07-23 23:46:34 -0400

    2-6

--- Chapter2/2-6.c
+++ Chapter2/2-6.c
@@ -3,10 +3,20 @@
 unsigned setbits(unsigned x, int p, int n, unsigned y);
 
 int main(void){
-	printf("%d\n", setbits(0xFFFF, 11, 8, 0));
+	printf("%4x\n", setbits(0xFFFF, 11, 8, 0));
+	printf("%4x\n", setbits(0xFFFF, 7, 8, 0));	
+	printf("%4x\n", setbits(0xFFFF, 15, 8, 0));
+	printf("%4x\n", setbits(0xFFFF, 11, 0, 0));
+	printf("%4x\n", setbits(0xFFFF, 11, 4, 0));
+	
 }
 
 unsigned setbits(unsigned x, int p, int n, unsigned y){
 	y << (p -n - 1);
-	int mask = ~(~0 << (p + 1));
+	unsigned mask = ~(~0 << (p + 1)); // mask is used to shift the relevant bits in y to position p
+	unsigned mask2 = (~0 << (p + 1)) | ~(~0 << p - n + 1); //mask2 is used to set the relevant bits in x to 0
+	y = y & mask;
+	x = x & mask2;
+	return y | x; 
+	
 }
