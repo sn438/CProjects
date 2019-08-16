c65a0ead5a039905601a68ba9d37ba7de31664e0
sn438 <sn438@cornell.edu>
2019-08-16 15:54:50 -0400

    4-12

--- Chapter4/4-12.c
+++ Chapter4/4-12.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
 
-void itoa(int n);
+int itoa(int n, char s[], int i);
 
 int sindex = 0;
 char s[10000];
 
 int main(void){
-	itoa(5);
+	itoa(5, s, 0);
 	printf("%s\n", s);
-	sindex = 0;
-	itoa(-5);
+	itoa(-5, s, 0);
 	printf("%s\n", s);
-	sindex = 0;
-	itoa(305);
+	itoa(305, s, 0);
 	printf("%s\n", s);
-	sindex = 0;
-	itoa(-4567);
+	itoa(-4567, s, 0);
 	printf("%s\n", s);
 	return 0;
 }
 
-void itoa(int n){
+int itoa(int n, char s[], int i)  {
 	if (n < 0) {
- 		s[sindex++] = '-';
+ 		s[i++] = '-';
 		n = -n;
  	}	
 	if (n / 10)
- 		itoa(n / 10);
-	s[sindex++] = n % 10 + '0';
-	s[sindex] = '\0';
+ 		i = itoa(n / 10, s, i);
+	s[i++] = n % 10 + '0';
+	s[i] = '\0';
+	return i;
 }
 
---
+++ Chapter4/4-13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int sindex = 0;
+
+int main(void){
+	return 0;
+}
+
+
+void reverse(char s[]){
+		
+}
